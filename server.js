6b0789da8bdbafe83886fa276ba214513dfb2b1c
Sivav15 <sivanathanv1999@gmail.com>
2024-07-21 18:41:32 +0530

    add database connection and folder structure.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const database = require("./src/config/database");
 
 dotenv.config();
 
@@ -8,11 +9,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+database();
+
 app.get("/", (req, res) =>
   res.status(200).json({
     message: "server is running successful",
   })
 );
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
